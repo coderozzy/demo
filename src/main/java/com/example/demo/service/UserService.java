4ca79685337d72a6fc52c828a9c8a466bc975ee8
coderozzy <110554261+coderozzy@users.noreply.github.com>
2025-01-25 18:18:16 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,29 @@
+package com.example.demo.service;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(String username, String email, String password) {
+        User user = new User(username, email, password);
+        return userRepository.save(user);
+    }
+
+    public boolean authenticateUser(String email, String password) {
+        Optional<User> user = userRepository.findByEmail(email);
+        return user.isPresent() && user.get().getPassword().equals(password);
+    }
+}
